10d6a4eb4e14c61751f58720ea30ecc8c989b519
Michael Frieze <michaelfrieze@icloud.com>
2021-11-26 16:32:29 -0500

    added some notes about adding dot server to filenames as a hint to the compiler that it is a server file

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -6,6 +6,10 @@ declare global {
   var __db: PrismaClient | undefined;
 }
 
+// db.server.ts
+// the .server is a hint to the compiler that this is a server file
+// It tells the compiler to not worry about this module or its imports when bundling for the browser
+
 // this is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
