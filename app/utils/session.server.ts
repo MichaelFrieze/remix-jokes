8b2e6a2a360cb90114a7bab0d3e23aec74077a44
Michael Frieze <michaelfrieze@icloud.com>
2021-11-29 16:27:31 -0500

    added a createUserSession function that creates a new session & sets the userId field on the session & redirects to the given route setting the set-cookie header

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import { createCookieSessionStorage, redirect } from 'remix';
 import { db } from './db.server';
 
 type LoginForm = {
@@ -11,9 +12,37 @@ export async function login({ username, password }: LoginForm) {
     where: { username },
   });
   if (!user) return null;
-
   let isCorrectPassword = await bcrypt.compare(password, user.passwordHash);
   if (!isCorrectPassword) return null;
-
   return user;
+}
+
+let sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error('SESSION_SECRET must be set');
+}
+
+let storage = createCookieSessionStorage({
+  cookie: {
+    name: 'RJ_session',
+    // normally you want this to be `secure: true`
+    // but that doesn't work on localhost for Safari
+    // https://web.dev/when-to-use-local-https/
+    secure: process.env.NODE_ENV === 'production',
+    secrets: [sessionSecret],
+    sameSite: 'lax',
+    path: '/',
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+});
+
+export async function createUserSession(userId: string, redirectTo: string) {
+  let session = await storage.getSession();
+  session.set('userId', userId);
+  return redirect(redirectTo, {
+    headers: {
+      'Set-Cookie': await storage.commitSession(session),
+    },
+  });
 }
