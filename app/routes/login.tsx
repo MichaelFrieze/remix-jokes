1020d3b60fc90bce70862962b13c95e335b0ffd0
Michael Frieze <michaelfrieze@icloud.com>
2021-12-02 15:33:40 -0500

    added Meta component to root module and exporting MetaFunction in routes for SEO

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,11 +1,18 @@
-import type { ActionFunction, LinksFunction } from 'remix';
-import { useActionData, useSearchParams, Link } from 'remix';
+import type { ActionFunction, LinksFunction, MetaFunction } from 'remix';
+import { useActionData, Link, useSearchParams } from 'remix';
 import { db } from '~/utils/db.server';
 import { createUserSession, login, register } from '~/utils/session.server';
 import stylesUrl from '../styles/login.css';
 
-export let links: LinksFunction = () => {
+export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: stylesUrl }];
+};
+
+export const meta: MetaFunction = () => {
+  return {
+    title: 'Remix Jokes | Login',
+    description: 'Login to submit your own jokes to Remix Jokes!',
+  };
 };
 
 function validateUsername(username: unknown) {
@@ -33,14 +40,14 @@ type ActionData = {
   };
 };
 
-export let action: ActionFunction = async ({
+export const action: ActionFunction = async ({
   request,
 }): Promise<Response | ActionData> => {
-  let form = await request.formData();
-  let loginType = form.get('loginType');
-  let username = form.get('username');
-  let password = form.get('password');
-  let redirectTo = form.get('redirectTo') || '/jokes';
+  const form = await request.formData();
+  const loginType = form.get('loginType');
+  const username = form.get('username');
+  const password = form.get('password');
+  const redirectTo = form.get('redirectTo') || '/jokes';
   if (
     typeof loginType !== 'string' ||
     typeof username !== 'string' ||
@@ -50,8 +57,8 @@ export let action: ActionFunction = async ({
     return { formError: `Form not submitted correctly.` };
   }
 
-  let fields = { loginType, username, password };
-  let fieldErrors = {
+  const fields = { loginType, username, password };
+  const fieldErrors = {
     username: validateUsername(username),
     password: validatePassword(password),
   };
@@ -59,7 +66,7 @@ export let action: ActionFunction = async ({
 
   switch (loginType) {
     case 'login': {
-      let user = await login({ username, password });
+      const user = await login({ username, password });
       if (!user) {
         return {
           fields,
@@ -69,7 +76,7 @@ export let action: ActionFunction = async ({
       return createUserSession(user.id, redirectTo);
     }
     case 'register': {
-      let userExists = await db.user.findFirst({
+      const userExists = await db.user.findFirst({
         where: { username },
       });
       if (userExists) {
@@ -94,8 +101,8 @@ export let action: ActionFunction = async ({
 };
 
 export default function Login() {
-  let actionData = useActionData<ActionData | undefined>();
-  let [searchParams] = useSearchParams();
+  const actionData = useActionData<ActionData | undefined>();
+  const [searchParams] = useSearchParams();
   return (
     <div className="container">
       <div className="content" data-light="">
